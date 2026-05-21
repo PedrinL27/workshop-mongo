d5f7641ea864de9ab63b5cb4cd9efffe521b8b11
PedrinL27 <pedrolangelineto0@gmail.com>
2026-05-21 16:23:36 -0300

    Return 404 not found if the id of the user not exist

--- src/main/java/com/pedroneto/workshopmongo/resources/UserResource.java
+++ src/main/java/com/pedroneto/workshopmongo/resources/UserResource.java
@@ -5,6 +5,7 @@ import com.pedroneto.workshopmongo.dto.UserDTO;
 import com.pedroneto.workshopmongo.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -27,6 +28,13 @@ public class UserResource {
                 .toList();
 
         return ResponseEntity.ok().body(listDTO);
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    public ResponseEntity<UserDTO> findAll(@PathVariable String id) {
+
+        User obj = service.findById(id);
+        return ResponseEntity.ok().body(new UserDTO(obj));
     }
 
 }

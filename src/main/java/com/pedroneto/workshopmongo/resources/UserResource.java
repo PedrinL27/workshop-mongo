267831052c03b36110463cb13bb881bf8fc1f9b9
PedrinL27 <pedrolangelineto0@gmail.com>
2026-05-21 15:54:55 -0300

    Added the database connection with a simple seeding of the database

--- src/main/java/com/pedroneto/workshopmongo/resources/UserResource.java
+++ src/main/java/com/pedroneto/workshopmongo/resources/UserResource.java
@@ -1,25 +1,29 @@
 package com.pedroneto.workshopmongo.resources;
 
 import com.pedroneto.workshopmongo.domain.User;
+import com.pedroneto.workshopmongo.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 @RestController
 @RequestMapping(value="/users")
 public class UserResource {
 
+    @Autowired
+    private UserService service;
+
+
     @RequestMapping(method = RequestMethod.GET)
     public ResponseEntity<List<User>> findAll() {
-        User maria = new User("3", "Maria Silva", "maria@email.com");
-        User alex = new User("1", "Alex Silva", "alex@email.com");
+        List<User> list = service.findAll();
 
-        List<User> list = new ArrayList<>(Arrays.asList(maria, alex));
+        System.out.println("Essa lista: " + list);
         return ResponseEntity.ok().body(list);
     }
+
 }

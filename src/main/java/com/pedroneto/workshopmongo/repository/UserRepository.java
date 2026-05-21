7499f302dba0e53ddd16c7daeaec0222adc772ed
PedrinL27 <pedrolangelineto0@gmail.com>
2026-05-21 15:55:06 -0300

    Added the database connection with a simple seeding of the database

---
+++ src/main/java/com/pedroneto/workshopmongo/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.pedroneto.workshopmongo.repository;
+
+import com.pedroneto.workshopmongo.domain.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String> {
+
+}

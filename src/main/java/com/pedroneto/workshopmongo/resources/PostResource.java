63648484a9d4a76e6597bd934b2dde80a842de50
PedrinL27 <pedrolangelineto0@gmail.com>
2026-05-22 14:08:21 -0300

    Full Search added

--- src/main/java/com/pedroneto/workshopmongo/resources/PostResource.java
+++ src/main/java/com/pedroneto/workshopmongo/resources/PostResource.java
@@ -1,10 +1,14 @@
 package com.pedroneto.workshopmongo.resources;
 
 import com.pedroneto.workshopmongo.domain.Post;
+import com.pedroneto.workshopmongo.resources.util.URL;
 import com.pedroneto.workshopmongo.services.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Date;
+import java.util.List;
 
 @RestController
 @RequestMapping(value="/posts")
@@ -15,8 +19,37 @@ public class PostResource {
 
     @RequestMapping(value = "/{id}", method = RequestMethod.GET)
     public ResponseEntity<Post> findById(@PathVariable String id) {
-
         Post obj = service.findById(id);
         return ResponseEntity.ok().body(obj);
+    }
+
+    @RequestMapping(value = "/titlesearch", method = RequestMethod.GET)
+    public ResponseEntity<List<Post>> findByTitle(@RequestParam(value = "text", defaultValue = "") String text) {
+        text = URL.decodeParam(text);
+
+        List<Post> list = service.findByTitle(text);
+        return ResponseEntity.ok().body(list);
+    }
+
+    @RequestMapping(value = "/searchbody", method = RequestMethod.GET)
+    public ResponseEntity<List<Post>> searchBody(@RequestParam(value = "text", defaultValue = "") String text) {
+        text = URL.decodeParam(text);
+
+        List<Post> list = service.searchBody(text);
+        return ResponseEntity.ok().body(list);
+    }
+
+    @RequestMapping(value = "/fullsearch", method = RequestMethod.GET)
+    public ResponseEntity<List<Post>> fullsearch(
+            @RequestParam(value = "text", defaultValue = "") String text,
+            @RequestParam(value = "minDate", defaultValue = "") String minDate,
+            @RequestParam(value = "maxDate", defaultValue = "") String maxDate) {
+
+        text = URL.decodeParam(text);
+        Date min = URL.convertDate(minDate, new Date(0L));
+        Date max = URL.convertDate(maxDate, new Date());
+
+        List<Post> list = service.fullSearch(text, min, max);
+        return ResponseEntity.ok().body(list);
     }
 }
--- src/main/java/com/pedroneto/workshopmongo/services/PostService.java
+++ src/main/java/com/pedroneto/workshopmongo/services/PostService.java
@@ -6,6 +6,8 @@ import com.pedroneto.workshopmongo.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -17,6 +19,19 @@ public class PostService {
     public Post findById(String id) {
         Optional<Post> obj = repo.findById(id);
         return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+    }
+
+    public List<Post> findByTitle(String text) {
+        return repo.findByTitleContainingIgnoreCase(text);
+    }
+
+    public List<Post> searchBody(String text) {
+        return repo.searchBody(text);
+    }
+
+    public List<Post> fullSearch(String text, Date minDate, Date maxDate) {
+        maxDate = new Date(maxDate.getTime() + 24 * 60 * 60 * 1000);
+        return repo.fullSearch(text, minDate, maxDate);
     }
 }
 

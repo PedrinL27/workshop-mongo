572acf938349a73faf10fc31b3fe85e760d97152
PedrinL27 <pedrolangelineto0@gmail.com>
2026-05-22 12:06:14 -0300

    Added the post entity

---
+++ src/main/java/com/pedroneto/workshopmongo/repository/PostRepository.java
@@ -0,0 +1,10 @@
+package com.pedroneto.workshopmongo.repository;
+
+import com.pedroneto.workshopmongo.domain.Post;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+    public interface PostRepository extends MongoRepository<Post, String> {
+
+}
